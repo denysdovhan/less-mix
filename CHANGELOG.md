446fe6e3f9c6a02069405b89cc0875f1c55363d5
Denys Dovhan <email@denysdovhan.com>
2016-02-06 15:27:01 +0200

    Split to mixins.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,13 @@
+# v3.0.0
+- Перехід до семантичного версіонування.
+- Додано цикл .for().
+- Видалено домішку `size()`, оскільки вона не практична.
+- Домішку бордер-радіус видалено, оскільки вона підримується браузерами: Android 2.1+, Chrome, Firefox 4+, IE 9+, iOS 4+, Opera 10.50+, Safari 5+.
+- Виправлені помилки з домішкою `.hide-text()` через які зникав фон, рамки та тіні контейнера.
+- Домішка `.test-border()` тепер може приймати параметр, що містить колір рамки.
+- `.background-clip()` видалено, оскільки він підтримується Android 2.2+, Chrome, Firefox 4+, IE 9+, iOS 4+, Opera 10.50+, Safari 4+
+
+
 # v2.0
 - **Major changes in gradient tools:**
 From now on appeal to the gradient occurs through `#gradient>.vertical()`, `#gradient>.hotrizontal()`, `#gradient>.directional()`, `#gradient>.radial()`
--- Gruntfile.js
+++ Gruntfile.js
@@ -1,105 +1,110 @@
 module.exports = function(grunt) {
 
-	// Project configuration.
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
-		// Compile less-files
-		less: {
-			// Task for development
-			development: {
-				files: {
-					// target.css file: source.less file
-					'docs/dist/css/styles.css': 'docs/source/less/styles.less'
-				}
-			},
-			// Release task
-			release: {
-				options: {
-					compress: true,
-					yuicompress: true
-				},
-				files: {
-					// target.css file: source.less file
-					'docs/dist/css/styles.css': 'docs/source/less/styles.less'
-				}
-			}
-		},
+    // Compile less-files in `docs`
+    less: {
+      // Task for development
+      docs: {
+        files: {
+          // target.css file: source.less file
+          'docs/dist/css/styles.css': 'docs/source/less/styles.less'
+        }
+      },
+      mixins: {
+        files: {
+          'mixins/tests/result.css': 'mixins/tests/tests.less',
+        }
+      },
+      // Release task
+      release: {
+        options: {
+          compress: true,
+          yuicompress: true
+        },
+        files: {
+          // target.css file: source.less file
+          'docs/dist/css/styles.css': 'docs/source/less/styles.less'
+        }
+      }
+    },
 
-		// Copy scripts and icons to `dest` folder
-		copy: {
-			js: {
-				files: [{
-					cwd: 'docs/source/js',
-					src: ['**/*.js'],
-					dest: 'docs/dist/js',
-					expand: true
-				}]
-			},
-			icons: {
-				files: [{
-					cwd: 'docs/source/ico',
-					src: ['**/*.png'],
-					dest: 'docs/dist/ico',
-					expand: true
-				}]
-			}
-		},
+    // Copy scripts and icons to `docs/dest` folder
+    copy: {
+      js: {
+        files: [{
+          cwd: 'docs/source/js',
+          src: ['**/*.js'],
+          dest: 'docs/dist/js',
+          expand: true
+        }]
+      },
+      icons: {
+        files: [{
+          cwd: 'docs/source/ico',
+          src: ['**/*.png'],
+          dest: 'docs/dist/ico',
+          expand: true
+        }]
+      }
+    },
 
-		// Run site on http://localhost:3000/
-		connect: {
-			server: {
-				options: {
-					hostname: 'localhost',
-					port: 3000,
-					base: 'docs/dist',
-				}
-			}
-		},
+    // Run `docs` on http://localhost:3000/
+    connect: {
+      server: {
+        options: {
+          hostname: 'localhost',
+          port: 3000,
+          base: 'docs/dist',
+        }
+      }
+    },
 
-		// Task that insert GoogleAnalytics append `body`
-		dom_munger: {
-			release: {
-				options: {
-					append: {
-						selector:'body',
-						html:'\t\t<script>(function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)	})(window,document,\'script\',\'//www.google-analytics.com/analytics.js\',\'ga\');	ga(\'create\', \'UA-40496885-2\', \'denysdovhan.github.io\');	ga(\'send\', \'pageview\');</script>\n'
-					},
-				},
-				src: 'docs/source/index.html',
-				dest: 'docs/dist/index.html'
-			}
-		},
+    // Task that insert GoogleAnalytics append `body`
+    dom_munger: {
+      release: {
+        options: {
+          append: {
+            selector:'body',
+            html:'\t\t<script>(function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,\'script\',\'//www.google-analytics.com/analytics.js\',\'ga\');  ga(\'create\', \'UA-40496885-2\', \'denysdovhan.github.io\'); ga(\'send\', \'pageview\');</script>\n'
+          },
+        },
+        src: 'docs/source/index.html',
+        dest: 'docs/dist/index.html'
+      }
+    },
 
-		// Watch task for development
-		watch: {
-			development: {
-				files: ['docs/source/**/*'],
-				tasks: ['less:development', 'copy']
-			}
-		}
-	});
+    // Watch task for development
+    watch: {
+      development: {
+        files: ['docs/source/**/*'],
+        tasks: ['less:development', 'copy']
+      }
+    }
+  });
 
-	// Load the plugins
-	grunt.loadNpmTasks('grunt-contrib-less');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-copy');
-	grunt.loadNpmTasks('grunt-contrib-connect');
-	grunt.loadNpmTasks('grunt-dom-munger');
+  // Load the plugins
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-dom-munger');
 
-	// Default task
-	grunt.registerTask('default', [
-		'connect',
-		'less:development',
-		'copy',
-		'watch'
-	]);
+  // Default task
+  grunt.registerTask('default', [
+    'connect',
+    'less:development',
+    'copy',
+    'watch'
+  ]);
 
-	// Release task
-	grunt.registerTask('release', [
-		'less:release',
-		'copy',
-		'dom_munger'
-	]);
+  // Release `docs` task 
+  grunt.registerTask('release', [
+    'less:release',
+    'copy',
+    'dom_munger'
+  ]);
 
 };
